8798ed113ab838fe43996fbd3f785bd68b0e5478
Morning <morning@mstars-lab4-1s-iMac.local>
2022-11-28 12:36:40 +0800

    Day10

---
+++ Day10/JS/main.js
@@ -0,0 +1,77 @@
+// var path = new Path(10, 200);
+// // Give the stroke a color
+// path.strokeColor = 'black';
+// var start = new Point(100, 100);
+// // Move to start and draw a line from there
+// path.moveTo(start);
+// // Note the plus operator on Point objects.
+// // PaperScript does that for us, and much more!
+// path.lineTo(start + [100, -50]);
+
+
+
+
+// var topLeft = new Point(150, 100);
+// var rectSize = new Size(200, 100);
+// var rect = new Rectangle(topLeft, rectSize);
+// var path = new Path.Rectangle(rect, 4);
+// path.fillColor = 'red';
+
+
+
+
+
+var hello ;
+console.log (hello)
+
+hello = "hello World from JS!!!"
+console.log(hello)
+
+hello = 'Here is now apple'
+console.log(hello)
+
+let myNumber ;
+console.log(myNumber);
+
+
+myNumber = 5;
+console.log(myNumber)
+
+let x = 8, y=6; z= 18
+
+console.log(x+y)
+
+console.log(x+y+15);
+
+console.log(x * x + y * y)
+
+const PI=3.14;
+
+console.log(PI)
+
+// PI=6.24; => const error
+
+// console.log(PI)
+
+var a =6;
+
+console.log (a)
+
+let b= 6;
+ b = 8;
+console.log(b)
+
+let plus = " + "
+let it = "It's "
+let sum = b + a
+
+let message = 'Hi, I added '
+console.log(message)
+
+let message1 = 'It is '
+
+console.log(message + b + a);
+
+
+alert(message + a + plus + b )
+alert(it + sum)
