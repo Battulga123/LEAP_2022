1068e8aaac6e1a0927d356e51511647c662e3dc5
Morning <morning@mstars-lab4-1s-iMac.local>
2022-11-29 12:31:36 +0800

    s

---
+++ Day11/main.js
@@ -0,0 +1,197 @@
+// let a = 'Baby'
+// let b = 'Shark,'
+// let c = 'doo-doo,'
+// let d = 'doo-doo'
+// let e = 'Mommy'
+// let f = 'Daddy'
+
+
+// console.log(a,b,c,d)
+// console.log(a,b,c,d)
+// console.log(a,b,c,d)
+// console.log(a,b)
+// console.log(e,b,c,d)
+// console.log(e,b,c,d)
+// console.log(e,b,c,d)
+// console.log(e,b)
+// console.log(f,b,c,d)
+// console.log(f,b,c,d)
+// console.log(f,b,c,d)
+// console.log(f,b)
+
+// let myNumber = 5;
+// let undify;
+// let nullVariable = null;
+// let myDesicion = true
+
+
+// // typeof
+
+// console.log(typeof 1);
+// console.log(typeof "Hello");
+// console.log(typeof undify);
+// console.log(typeof nullVariable);
+// console.log(myDesicion)
+// console.log(typeof myDesicion)
+
+// // EX. Alert
+
+// let x = 4;
+// let y = 5 ;
+// let z = 3
+
+// // alert((x * x) + (y * y) - (z * z * z) / 240 * 75)
+
+
+// console.log(7%2)
+// console.log(36%19)
+// let result = '30' - 10
+// console.log(result)
+
+// // EX. Toirgiin urt
+
+// const PI=3.14;
+// let R = 5;
+// console.log(PI * 2 * R)
+
+
+// // EX. Trapezoid 
+
+
+// let q = 3
+// let w = 4
+// let h = 5
+// console.log ((q + w)/2*h)
+
+// let bottom = prompt('Please give me your bottom')
+// let test = prompt('Please give me your test')
+// let height = prompt('Please give me your height')
+// let trapezoid = (bottom + test)/2 * height
+// alert(trapezoid)
+
+
+// // if condition
+
+// let a = prompt('Please give me a')
+// let b = prompt('Please give me b')
+
+// if (a > b) {
+//     console.log('its ture')
+// } else {
+//     console.log('its false')
+// }
+
+
+// // **** EX.YearofBirth(and &&)
+
+
+// let yearofBirth = prompt('give birth date')
+
+// let old = 2022 - yearofBirth
+
+// console.log(old)
+
+// if (0 <= old && old <= 1) {
+//     console.log('Infant')
+// }
+// else if (1 < old && old <= 3) {
+//     console.log('Toddler')
+// }
+// else if (3 < old && old <= 5) {
+//     console.log('Preschool')
+// }
+// else if (5 < old && old <= 12) {
+//     console.log('Gradeschool')
+// }
+// else if (12 < old && old <= 18) {
+//     console.log('Teen')
+// }
+// else if (18 < old && old <= 21) {
+//     console.log('Young adult')
+// }
+// else {
+//     console.log('Adult')
+// }
+
+
+// // EX. Өгөгдсөн тооны тэгш эсвэл сондгой болохыг хэвлэж харуулна уу
+
+// let number = prompt('give number')
+// console.log(number);
+// let result = number%2
+// console.log(result)
+
+// if (result == 0 ) {
+//     console.log('tegsh')
+// } else {
+//     console.log('sondgoi')
+// }
+
+
+// // EX. Өгөгдсөн эерэг тоо нь 3-ын үржвэр эсвэл 7-ын үржвэр эсэхийг шалгана уу
+
+// let number = prompt('give number')
+// console.log(number)
+// if (number%3 == 0 || number%7 == 0) {
+// console.log('OK')
+// } else {
+//     console.log('NO')
+// }
+
+// // EX. weight, height гэсэн 2 хувьсагчид утга оруулахад тухайн хэмжээнээс хамааран биеийн жингийн индексийг тооцож тухайн индекс-д харгалзах утгыг хэвлэнэ. 
+
+// let weight = prompt('give weight')
+// let height = prompt('give height')
+// let BMI = weight / (height * height)
+// console.log(BMI)
+// if (BMI < 18.5) {
+//     console.log('Under weight')
+// } else if (18.5 <= BMI && BMI < 25) {
+//     console.log('Normal')
+// } else if (25 <= BMI && BMI < 30) {
+//     console.log('Over Weight')
+// } else if (30 <= BMI && BMI < 35) {
+//     console.log('Obesity(Class I)')
+// } else if (35 <= BMI && BMI < 40) {
+//     console.log('Obesity(Class II)')
+// } else {
+//     console.log('Obesity Obesity')
+// }
+
+
+// // EX. Switch 
+
+// let day = prompt('give day')
+// let dayNumber = Number(day)
+// let dayName
+
+// switch (dayNumber) {
+//     case 1:
+//         dayName = 'Monday'
+//         break;
+//     case 2:
+//         dayName = "Tuesday"
+//         break;
+//     case 3:
+//         dayName = "Wednesday"
+//         break;
+//     case 4:
+//         dayName = "Thusday"
+//         break;
+//     case 5:
+//         dayName = "Friday"
+//         break;
+//     case 6:
+//         dayName = "Saturday"
+//         break;
+//     case 7:
+//         dayName = "Sunday"
+//         break;
+//     default:
+//         dayName = "Invalid day"
+// }
+// console.log (dayName)
+
+// // EX. n  хувьсагчын утгын дагуу эерэг эсвэл сөрөгийг олж хэвлэнэ.
+
+
