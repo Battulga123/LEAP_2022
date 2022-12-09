11e2ae258782b7a6870d4099c9d2e6d0e88239c0
Morning <morning@mstars-lab4-1s-iMac.local>
2022-12-09 10:10:31 +0800

    Day19

---
+++ Day19/Assignment.js
@@ -0,0 +1,30 @@
+function howDoesTheFoxSay(array, animaltype) {
+    for (let i = 0; i < array.length; i++) {
+       if (array[i].type == animaltype) {
+        console.log(array[i].sound, '-',array[i].sound,'-',array[i].sound )
+       } 
+    }
+}
+
+let animals = [
+    {
+        type: 'dog',
+        sound: 'woof'
+    }
+    ,
+    {
+        type: 'cat',
+        sound: 'meow'
+    },
+    {
+        type: 'cow',
+        sound: 'moo'
+    }
+]
+
+console.log('Dog barking')
+howDoesTheFoxSay(animals,'dog')
+console.log('Cow mooing')
+howDoesTheFoxSay(animals,'cow')
+console.log('cat meowing')
+howDoesTheFoxSay(animals,'cat')
